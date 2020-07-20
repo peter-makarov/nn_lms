5eb42cc07d67e418cd973d820a98f8a4d8a39f95
Peter Makarov <makarov@ifi.uzh.ch>
2020-07-20 23:28:26 +0200

    fix packaging

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup
 
 setup(name='nn_lm',
       version='0.1',
-      description='Word and character neural network language models',
+      description='Word and character neural network language models with flair and pytorch',
       url='',
       author='Peter Makarov & Simon Clematide',
       author_email='makarov@cl.uzh.ch',
       license='MIT',
       packages=['nn_lm'],
-      zip_safe=False)
+      install_requires=[
+          "flair==0.4.1",
+          "numpy==1.16.3",
+          "sacred==0.7.4",
+          "torch==1.0.1.post2",
+      ],
+      zip_safe=True)
 
